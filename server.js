0341a17bbe965dae612002459221393a21f4fecb
leavesza <yaseenlevy21@gmail.com>
2023-08-12 01:00:04 +0200

    Added pg DB Refactored code

--- server.js
+++ server.js
@@ -2,8 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
+const knex = require('knex');
 
+const register = require('./controllers/register');
+const signin = require('./controllers/signin');
+const profile = require('./controllers/profile');
+const image = require('./controllers/image');
 
+const db = knex({
+    client: 'pg',
+    connection: {
+      host : '127.0.0.1',
+      port : 5432,
+      user : 'postgres',
+      password : 'test',
+      database : 'smart-brain'
+    }
+  });
+
+db.select('*').from('users').then(data => {
+    console.log(data);
+});
+
+//refresh 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
@@ -36,68 +57,15 @@ const database = {
     ]
 }
 
-app.get('/', (req,res) =>{
-    res.send(database.users);
-})
+app.get('/', (req,res) =>{ res.send('success')})
 
-app.post('/signin',(req,res) =>{
-    // Load hash from your password DB.
-bcrypt.compare("apples", '$2a$10$uTjYM7Z6uL0H0ILYBjkvp.RxD6JC2PTKqY46sSAfEOSw4pwCUYogO', function(err, res) {
-    console.log('first guess',res);
-});
-bcrypt.compare("veggies", '$2a$10$uTjYM7Z6uL0H0ILYBjkvp.RxD6JC2PTKqY46sSAfEOSw4pwCUYogO', function(err, res) {
-    console.log('second guess',res);
-});
-    if(req.body.email === database.users[0].email && 
-        req.body.password === database.users[0].password){
-        res.json(database.users[0]);
-    }
-    else {
-        res.status(400).json('error logging in');
-    }
-})
+app.post('/signin', signin.handleSignIn(db, bcrypt))
 
-app.post('/register',(req,res) =>{
-    const { email, name, password } = req.body;
-    
-    database.users.push({
-        id: '125',
-        name: name,
-        email: email,
-        entries: 0,
-        joined: new Date()
-    })
-    res.json(database.users[database.users.length - 1]);
-})
+app.post('/register',(req,res) => {register.handleRegister(req, res, db, bcrypt)})
 
-app.get('/profile/:id',(req,res)=>{
-    const { id } = req.params;
-    let found = false;
-    database.users.forEach(user =>{
-        if (user.id === id){
-            found = true;
-            return res.json(user);
-        }
-    })
-    if (!found){
-        res.status(400).json('not found');
-    }
-})
+app.get('/profile/:id',(req, res) => { profile.handleProfileGet(req, res, db)})
 
-app.put('/image', (req,res)=>{
-    const { id } = req.body;
-    let found = false;
-    database.users.forEach(user =>{
-        if (user.id === id){
-            found = true;
-            user.entries++;
-            return res.json(user.entries);
-        }
-    })
-    if (!found){
-        res.status(400).json('not found');
-    }
-})
+app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 
 
 
