e5997d41e0c3bb936d5f861541b52ec44081c0ca
leavesza <yaseenlevy21@gmail.com>
2023-08-15 01:17:29 +0200

    fix

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db)})
 
 
 
-app.listen(process.env,PORT || 3000, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log('app is running on port 3000');
 })
 
